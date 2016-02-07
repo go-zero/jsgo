3b2d86b79cfd804999ed1c80f4f98479b8f7b1c3
Jairo Luiz <t4ngz3r0@gmail.com>
2016-02-06 23:45:33 -0300

    better JsValue tests

--- js_value_test.go
+++ js_value_test.go
@@ -15,52 +15,61 @@ var _ = Describe("JsValue", func() {
 	state := NewJsState()
 
 	It("Should return a float", func() {
-		value, _ := state.DoString("182.0")
+		value, err := state.DoString("182.0")
+		Expect(err).ToNot(HaveOccurred())
 		Expect(value.Float()).To(Equal(182.0))
 	})
 
 	It("Should return an integer", func() {
-		value, _ := state.DoString("42")
+		value, err := state.DoString("42")
+		Expect(err).ToNot(HaveOccurred())
 		Expect(value.Integer()).To(Equal(42))
 	})
 
 	It("Should return a string", func() {
-		value, _ := state.DoString("'Hello World!'")
+		value, err := state.DoString("'Hello World!'")
+		Expect(err).ToNot(HaveOccurred())
 		Expect(value.String()).To(Equal("Hello World!"))
 	})
 
 	It("Should return a boolean", func() {
-		value, _ := state.DoString("true")
+		value, err := state.DoString("true")
+		Expect(err).ToNot(HaveOccurred())
 		Expect(value.Bool()).To(BeTrue())
 	})
 
 	It("Should return a callable", func() {
-		value, _ := state.DoString("new Function()")
+		value, err := state.DoString("new Function()")
+		Expect(err).ToNot(HaveOccurred())
 		_, ok := value.(*JsCallable)
 		Expect(ok).To(BeTrue())
 	})
 
 	It("Should return an object", func() {
-		value, _ := state.DoString("new Object()")
+		value, err := state.DoString("new Object()")
+		Expect(err).ToNot(HaveOccurred())
 		_, ok := value.(*JsObject)
 		Expect(ok).To(BeTrue())
 	})
 
 	It("Should return an array", func() {
-		value, _ := state.DoString("new Array()")
+		value, err := state.DoString("new Array()")
+		Expect(err).ToNot(HaveOccurred())
 		_, ok := value.(*JsArray)
 		Expect(ok).To(BeTrue())
 	})
 
 	It("Should return a regexp", func() {
-		value, _ := state.DoString("new RegExp()")
+		value, err := state.DoString("new RegExp()")
+		Expect(err).ToNot(HaveOccurred())
 		_, ok := value.(*JsRegExp)
 		Expect(ok).To(BeTrue())
 	})
 
 	DescribeTable("Should identify the value type",
 		func(code string, check CheckFunction) {
-			value, _ := state.DoString(code)
+			value, err := state.DoString(code)
+			Expect(err).ToNot(HaveOccurred())
 			Expect(check(value)).To(BeTrue())
 			runtime.GC() // force the unreferencing of the value
 		},
